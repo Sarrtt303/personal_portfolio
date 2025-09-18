dd5cfbc6bde790d204ef91c3923ce213dd8c0f16
Sagar Debnath <sagardebnath1001@gmail.com>
2025-09-18 15:06:44 +0530

    resume update

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -7,7 +7,7 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/cv(may2025).pdf";
+import CV from "../../assets/cv(Sept).pdf";
 import { useTheme } from "../../components/ThemeContext";
 
 function Hero() {
